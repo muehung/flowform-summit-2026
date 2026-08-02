d51487ce579c5d7c15b7df30423faa1ff85d3276
muehung <hung.co24@gmail.com>
2026-08-02 11:26:28 +0800

    chore: install pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import './main.css' // 有tailwind
+import { createPinia } from 'pinia'
 import router from './router/router.js'
 import App from './App.vue'
 
+const pinia = createPinia;
+
 createApp(App)
+    .use(pinia)
     .use(router)
     .mount('#app')
