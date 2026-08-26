47748d2270b2ef1706a53cc69012999f800d0655
muehung <hung.co24@gmail.com>
2026-08-26 18:07:43 +0800

    refactor(store): remove useStorage, stop storing password for security

--- frontend/src/stores/useFormStore.js
+++ frontend/src/stores/useFormStore.js
@@ -1,7 +1,6 @@
 import { ref, onMounted } from "vue"
 import { defineStore } from "pinia"
 import router from "../router/router";
-import { useStorage } from "@vueuse/core"
 
 const formDefault = {
         nameX: '',
@@ -15,14 +14,11 @@ const formDefault = {
         interests: [],
         //
         account:'',
-        password: '',
 }
 
 export const useFormStore = defineStore('formData', ()=>{
     const form = ref({...formDefault});
-    const STORAGE_KEY = ref("form-step1");
-    useStorage( STORAGE_KEY.value, form, localStorage, { mergeDefaults: true } ); 
-    // 第2參數本身有帶ref(), 第4參數mergeDefaults: true 新增欄位而 LocalStorage 的資料缺少該欄位，會自動把預設值補進去
+    
     const inputFirstFocus =  ref(null);
 
     const submitGoNext = function(num) {
@@ -32,7 +28,6 @@ export const useFormStore = defineStore('formData', ()=>{
 
     const cancelRegistration = function() {
         form.value = { ...formDefault };
-        // errors.value = { ...errorsDefault }
     }
 
     return { form, STORAGE_KEY, inputFirstFocus, submitGoNext, cancelRegistration }
