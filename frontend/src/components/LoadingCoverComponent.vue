56f18d8b87ded17abdefc57ec5904b5d82081a00
muehung <hung.co24@gmail.com>
2026-08-28 23:23:11 +0800

    feat: add LoadingCoverComponent

---
+++ frontend/src/components/LoadingCoverComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-white/90 z-50 w-100 h-100 fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+        <div class="animate-spin h-6 w-6 border-2 border-secondary border-t-transparent rounded-full"></div>
+    </div>
+</template>
