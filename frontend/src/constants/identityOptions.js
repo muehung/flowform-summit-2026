03a5ee6fee4c3ea52375b33436024d400cb93c26
muehung <hung.co24@gmail.com>
2026-09-02 17:59:22 +0800

    feat: add identityOptions data for Step01 & Step04 form

---
+++ frontend/src/constants/identityOptions.js
@@ -0,0 +1,42 @@
+export const identityOptions = [
+        {
+            id: 1,
+            value: "developer",
+            label: "軟體開發者 (Developer)",
+        },
+        {
+            id: 2,
+            value: "devops",
+            label: "維運工程師 (DevOps)"
+        },
+        {
+            id: 3,
+            value: "student",
+            label: "在學學生 (Student)"
+        },
+        {
+            id: 4,
+            value: "manager",
+            label: "技術經理 (Manager)"
+        },
+        {
+            id: 5,
+            value: "designer",
+            label: "設計師 (Designer)"
+        },
+        {
+            id: 6,
+            value: "vip",
+            label: "貴賓 (VIP)"
+        },
+        {
+            id: 7,
+            value: "speaker",
+            label: "講者 (Speaker)"
+        },
+        {
+            id: 8,
+            value: "public",
+            label: "民眾 (General Public)"
+        },
+];
